19bed1ed7a567330928cd773151a0c9664cf0633
zhongshexiang <zhongshexiang@126.com>
2016-08-30 18:17:30 +0800

    est

---
+++ UPSTAR.System.ModelMapper/ExtensionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UPSTAR.System.ViewModel;
+using CZBK.ItcastOA.Model;
+using AutoMapper;
+namespace UPSTAR.System.ModelMapper
+{
+    public static class ExtensionHelper
+    {
+        static ExtensionHelper()
+        {
+            Mapper.CreateMap<LoginViewModel, UserInfo>();
+        }
+
+        public static UserInfo UserInfoMap(this LoginViewModel model)
+        {
+            return Mapper.Map<LoginViewModel, UserInfo>(model);
+        }
+    }
+}
